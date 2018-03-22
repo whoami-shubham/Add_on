f1ab5f9130d848614ddaffa1ee102019c13c6a57
shubham <shubhamkumarjha0013@gmail.com>
2018-03-22 19:47:30 +0530

    add on files added

---
+++ app.js
@@ -0,0 +1,10 @@
+
+document.body.style.border = "5px solid red";
+//var list = document.getElementsByTagName("div")[0];
+//list.innerHTML = "<h1 style='text-align:center'>shubham !</h1>";
+var list2 = document.getElementsByTagName("div")[1];
+function myFunction(list2) {
+	list2.innerHTML =""
+}
+//list2.innerHTML ="<button onclick='myFunction()' style='background:black;color:green'>Click me</button>";
+document.body.style.backgroundImage = "url('https://svn.exotica.org.uk:8443/xbmc4xbox/branches/addons/addons/skin.confluence/backgrounds/addons.jpg')";
